c9c8490f813be480c456b3431cc22ae5397f8d59
✩°｡⋆⸜ TaXi0k ✮ ᯓ★ <igorciu@interia.pl>
2025-10-19 19:50:20 +0200

    mostly mobile updates

--- script/projects.js
+++ script/projects.js
@@ -1,7 +1,7 @@
 const projects = [
     { img: 'img/projects/mdl.png', name: 'MyDeathLink', desc: "A way to sync two games on two PCs so when you die, your friend also dies. ONLY ON WINDOWS!", href: 'https://www.youtube.com' },
     { img: 'img/projects/v.png', name: 'Victoria Sato - my V', desc: "Character sheet of my V, name Victoria Sato. Formerly 2nd most important figure in Arasaka Counterintelligence. Now...", href: 'https://www.youtube.com' },
-    { img: 'img/projects/1z10.png', name: 'MISIADOWE 1z10: Autyzm Edcyja', desc: 'App letting you play 1z10 (something like Fifteen to One) with your friends by streaming your screen.', href: 'https://www.youtube.com' },
+    { img: 'img/projects/1z10.png', name: 'MISIADOWE 1z10: AE', desc: 'App letting you play 1z10 (something like Fifteen to One) with your friends by streaming your screen.', href: 'https://www.youtube.com' },
 ]
 
 function loadProjects() {
@@ -20,7 +20,7 @@ function loadProjects() {
         info.className = 'projectInfo';
         info.innerHTML = `
             <div class="projectName">${project.name}</div>
-            <div class="projectDesc">${project.desc}</div>
+            <div class="projectDesc"><span>${project.desc}</span></div>
         `;
 
         item.appendChild(img);
